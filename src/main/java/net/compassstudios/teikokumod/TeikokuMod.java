10bb65a90b3d5a4841d0dadb8b075618d43cefb8
Sam Babb <compass.studios.main@gmail.com>
2025-04-12 03:15:29 -0700

    Initial commit

---
+++ src/main/java/net/compassstudios/teikokumod/TeikokuMod.java
@@ -0,0 +1,15 @@
+package net.compassstudios.teikokumod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TeikokuMod implements ModInitializer {
+	public static final String MOD_ID = "TeikokuMod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/compassstudios/teikokumod/TeikokuModClient.java
@@ -0,0 +1,10 @@
+package net.compassstudios.teikokumod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TeikokuModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
